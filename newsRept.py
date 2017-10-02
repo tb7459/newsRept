718f98226d4f06fbfd8511453d758d4eddb75941
tb7459 <31620023+tb7459@users.noreply.github.com>
2017-10-02 12:05:16 -0500

    Update newsRept.py

--- newsRept.py
+++ newsRept.py
@@ -4,30 +4,28 @@
 # 2) most popular authors, and
 # 3) date(s) with > 1% error
 
-from flask import Flask, request, redirect, url_for
 from newsReptdb import get_popular_articles, get_popular_authors, get_perc_bad
 
-app = Flask(__name__)
 articles = get_popular_articles()
 authors = get_popular_authors()
 errors = get_perc_bad()
 
-print " "
-print "Article data: "
+print (" ")
+print ("Article data: ")
 
 for article in articles:
-    print "  \"", article[0], "\" -- ", article[1], " views"
+    print ("  \"", article[0], "\" -- ", article[1], " views")
 
-print " "
-print "Author data: "
+print (" ")
+print ("Author data: ")
 
 for author in authors:
-    print "   ", author[0], " -- ", author[1], " views"
+    print ("   ", author[0], " -- ", author[1], " views")
 
-print " "
-print "Error data: "
+print (" ")
+print ("Error data: ")
 
 for error in errors:
-    print "    ", error[0], " -- ", error[1], " % errors"
+    print ("    ", error[0], " -- ", error[1], " % errors")
 
-print "  "
+print ("  ")
