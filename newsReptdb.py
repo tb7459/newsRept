c5ccbbf0c756351729409ae8c46ce3224b6a0856
tb7459 <31620023+tb7459@users.noreply.github.com>
2017-10-02 12:05:56 -0500

    Update newsReptdb.py

--- newsReptdb.py
+++ newsReptdb.py
@@ -1,24 +1,27 @@
 # Database code for the newsRept
 
 import psycopg2
-import bleach
 
 DBNAME = "news"
+
+
+def closeDB(db):
+    db.close()
 
 
 def get_popular_articles():
     """Return 3 most popular aritcles, most popular first."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    query = "SELECT art.title, count(log.path) as views "
-    query = query + "FROM log INNER JOIN articles art "
-    query = query + " ON art.slug = substring(log.path,10) "
-    query = query + "WHERE path != '/' "
-    query = query + "GROUP BY art.title "
-    query = query + "ORDER BY views desc limit 3;"
+    query = ("""SELECT art.title, count(log.path) as views
+                FROM log INNER JOIN articles art
+                 ON art.slug = substring(log.path,10)
+                WHERE path != '/'
+                GROUP BY art.title
+                ORDER BY views desc limit 3;""")
     c.execute(query)
     articles = c.fetchall()
-    db.close()
+    closeDB(db)
     return articles
 
 
@@ -27,15 +30,15 @@ def get_popular_authors():
     popular first."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    query = "SELECT authors.name as autName, count(log.id) as autViews "
-    query = query + "FROM log INNER JOIN articles art "
-    query = query + "on art.slug = substring(path,10) "
-    query = query + "INNER JOIN authors on authors.id = art.author "
-    query = query + "where path != '/' "
-    query = query + "group by authors.name order by autViews desc;"
+    query = ("""SELECT authors.name as autName, count(log.id) as autViews
+                FROM log INNER JOIN articles art
+                 ON art.slug = substring(path,10)
+                INNER JOIN authors on authors.id = art.author
+                WHERE path != '/'
+                group by authors.name order by autViews desc;""")
     c.execute(query)
     authors = c.fetchall()
-    db.close()
+    closeDB(db)
     return authors
 
 
@@ -44,16 +47,17 @@ def get_perc_bad():
     percentage of errors for the day was > 1% """
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    query = "SELECT  dateVal, round(prcVal,2) as prcVal "
-    query = query + "FROM (select dateVal, "
-    query = query + "((cast(badNum as decimal)/TotalNum) * 100) as prcVal "
-    query = query + "FROM (select a.dateVal, a.statCount as goodNum, "
-    query = query + "b.statCount as badNum, "
-    query = query + "(a.statCount + b.statCount) as TotalNum "
-    query = query + "FROM vw_date_statCount a join vw_date_statCount b "
-    query = query + "on a.dateVal = b.dateVal "
-    query = query + "and a.statCount > b.statCount) as percQ) "
-    query = query + " as summQ WHERE prcVal > 1.0;"
+    query = ("""SELECT  dateVal, round(prcVal,2) as prcVal
+                FROM (select dateVal,
+                    ((cast(badNum as decimal)/TotalNum) * 100) as prcVal
+                      FROM (select a.dateVal, a.statCount as goodNum,
+                            b.statCount as badNum,
+                           (a.statCount + b.statCount) as TotalNum
+                      FROM vw_date_statCount a join vw_date_statCount b
+                       on a.dateVal = b.dateVal
+		       and a.status = '200 OK' 
+                       and b.status = '404 NOT FOUND') as percQ)
+                       as summQ WHERE prcVal > 1.0;""")
     c.execute(query)
     errors = c.fetchall()
     db.close()
